7a5f292931f2c19b408b0ecdfad0f801bb3fc28d
guillermo <navarro.g.guillermo@gmail.com>
2015-07-08 20:00:43 -0400

    doxygen

--- home.cpp
+++ home.cpp
@@ -1,151 +1,339 @@
+/**
+	* @file main.cpp
+	* @version 2.0
+	* @date 10/04/2015
+	* @author Alan Collante & Guillermo Navarro
+	* @bug Puntos que esten proximos al marco de la imagen produce errores
+	
+	main.cpp es un programa para traducir una imagen con sistema braille a caracteres
+
+	*/
+
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <stdio.h>
-#include <string>
-#include <vector>
+#include <stdlib.h>
+#include <algorithm>    // std::sort
 
-using namespace std;
 using namespace cv;
+using namespace std;
+/** * src Imagen cargada */
+Mat src; 
+/** * src_gray Imagen cargada a escala gris */
+Mat src_gray;
+/** * lineas Imagen cargada a escala gris con las verticales y horizonatles */
+Mat lineas;
+/** * tresh Valor del treshold */
+int thresh = 150;
+/** * tresh Valor maximo del treshold */
+int max_thresh = 255;
 
-Mat integralimage(Mat in){
-	Mat Int = in.clone();
-
-	for( int x = 0; x < in.rows; x++)
-	{
-		for( int y = 0; y < in.cols; y++)
-		{
-			uchar out = in.at<uchar>(x,y);
-
-			if( x - 1 >= 0 ) out += in.at<uchar>(x - 1,y);
-			if( y - 1 >= 0 ) out += in.at<uchar>(x, y - 1 );
-			if( x - 1 >= 0 && y - 1 >= 0 ) out -= in.at<uchar>(x - 1,y - 1);
-			Int.at<uchar>(x,y)= out;
-		
-		}
-	}
-	return Int;
-}
-
-Mat windo(Mat Int){
-	
-	Mat f = Int.clone();
-	 //supresion de no maximos
-	int c1=6;
-	float p = 0.15;
-
-	/*for(int r=c1/2;r< Int.rows-c1/2; r++){
-		for(int c=c1/2;c< Int.cols-c1/2; c++){
-			float s=0;
-			for(int x=-c1/2;x< (c1/2)+1; x++){
-				for(int y=c1/2;y< (c1/2)+1; y++){
-
-						//s += Int.at<char>(r,c);
-						s += Int.at<char>(r,c);
-						
-					}
-			}
-			//f.at<char>(r,c)= s/(c1*c1);
-			if ( f.at<char>(r,c) <= (s*(1.0-p))/(c1*c1)) f.at<char>(r,c)= 255;
-			else f.at<char>(r,c)= 0;
-		}
-	}
+/** 
+	* @brief Carga los caracteres en sistema braille a memoria
+	* @param abc Vector vacia de imagenes.
+	* @return abc Vector con las imagenes de los caracteres en sistema braille cargadas.
 	*/
-	/*for( int x = c1/2; x < Int.rows-c1/2; x++)
-	{
-		for( int y = c1/2; y < Int.cols-c1/2; y++)
-		{
-			f.at<char>(x,y)= Int.at<char>(x,y)- Int.at<char>(x+c1,y-c1-1) - Int.at<char>(x-c1-1,y+c1) + Int.at<char>(x+c1-1,y-c1-1);
-		}
-	}
+void getdb(vector<Mat> &abc);
+/** 
+	* @brief buscarverticales busca los verticales segun los puntos encontrados.
+	* @param puntos Puntos encontrados en la imagen.
+	* @return puntos retorna un vector con las verticales encontradas.
 	*/
+vector<int> buscarverticales(vector<Point2f> puntos);
+/** 
+	* @brief buscarhorizontales busca los horizontales segun los puntos encontrados.
+	* @param puntos Puntos encontrados en la imagen.
+	* @return puntos retorna un vector con las horizontales encontradas.
+	*/
+vector<int> buscarhorizontales(vector<Point2f> puntos);
+/** 
+	* @brief thresh_callback procesa la imagen cargada para traducirlo.
+	* @return Imagen traducida.
+	*/
+void thresh_callback(int, void* );
 
-	return f;
-}
-
+/** 
+	* @brief main
+	*/
 
 int main( int argc, char** argv )
 {
-  Mat img,img_g;
-  img = imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//test.jpg" ); 
-  cvtColor( img, img_g, COLOR_BGR2GRAY );
-  imshow("Imagen0", img_g );
+	/// Load source image
+	src = imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//test.jpg", 1 );
+	cvtColor( src, src_gray, CV_BGR2GRAY );
+	blur( src_gray, src_gray, Size(3,3) );
+	/// Create Window
+	char* source_window = "Source";
+	namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+	imshow( source_window, src );
 
-  //Mat integral = integralimage(img_g);
-  //imshow("Imagen1", integral );
- // Mat bin = windo(integral);
- // imshow("Imagen3", bin );
+	createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
+	thresh_callback( 0, 0 );
 
-  
-
-
-  //*********************
-
-  Mat dst;
-  Mat temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//b.jpg" ); 
-  Mat template1;
-  cvtColor( temp, template1, COLOR_BGR2GRAY );
-  Mat result;
-  threshold( img_g, dst, 150, 255, 1 );
-  imshow("Imagen4", dst );
-  imshow("Imagen7", template1 );
-  Mat f;
-  Mat ventana(50, 50, CV_8UC1);
-  //Mat ventana= Mat::ones(50, 50, CV_8UC1);
+	waitKey(0);
+	return(0);
+}
 
 
-  for( int x = 0; x < dst.rows; x++)
-  {
-		for( int y = 0; y < dst.cols; y++)
-		{
-			if(dst.at<uchar>(x,y) <= 100) {
-				//for( int i = 0; i < ventana.rows; i++){
-				//	for( int j = 0; j < ventana.cols; j++){
-				for( int i = 0; i < 25; i++){
-					for( int j = 0; j < 14; j++){
-						//IF (EN LA HORIZONTAL NO HAY MAS PUNTOS SE DESCARTA)
-						ventana.at<uchar>(i,j) = dst.at<uchar>(x+i,y+j);
-						dst.at<uchar>(x+i,y+j)=255;
-					}
+void thresh_callback(int, void* )
+{
+  Mat threshold_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
+  lineas= threshold_output.clone();
+  findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  vector<vector<Point> > contours_poly( contours.size() );
+  vector<Rect> boundRect( contours.size() );
+  vector<Point2f>center( contours.size() );
+  vector<float>radius( contours.size() );
+
+  for( int i = 0; i < contours.size(); i++ )
+  { 
+	   approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+       minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+  }
+
+  Mat drawing = Mat( threshold_output.size(), CV_8UC1, Scalar( 255 ) );
+  Scalar color = Scalar( 0,123,46 );
+
+  vector<vector<Point> > grupos;
+  for( int i = contours.size()-1; i>=0 ; i-- )
+     {
+	   if(radius[i] <4){
+			for( int x =  boundRect[i].tl().x; x < boundRect[i].tl().x+3; x++)
+			{
+				for( int y =  boundRect[i].tl().y; y <  boundRect[i].tl().y+5; y++)
+				{
+					if(x<drawing.cols && y < drawing.rows)
+					drawing.at<uchar>(y,x)=0;
 				}
+			}
+	   }
+	
+     }
 
-				matchTemplate( ventana, template1, result, 5 );
-
-				imshow("Imagen65", ventana );
-				imshow("Imagen5", result );
-				imshow("Imagen4", dst );
-
-				
-				for( int w = 0; w < result.rows; w++){
-					for( int h = 0; h < result.cols; h++){
-						if(result.at<float>(w,h) >= 0.55 ) {
-							cout<<"encontrado en pos "<<x <<" "<< y;
-
-							string text = "y";
-							int fontFace = FONT_HERSHEY_SIMPLEX;
-							double fontScale = 1;
-							int thickness = 3;  
-							cv::Point textOrg(y,x );
+  vector<int> horizontales = buscarhorizontales(center);
+  vector<int> verticales = buscarverticales(center);
 
 
-							cv::putText(img_g, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness,8);
+  string probando = "abcdefghijklmnopqrstuvwxyz";
+  vector<Mat> abc;
+  getdb(abc);
+  Mat ventana(50, 50, CV_8UC1,256);
+  Mat result;
+  int x;
 
+  for(int h = 0 ; h < horizontales.size() ; h++ ){
+	  if(h+1<horizontales.size())
+	  if(horizontales[h+1]-horizontales[h]>14)
+		continue;
+	  else{ 
+		  x=horizontales[h];
+		  h+=2;
+	   }
+	  for( int v = 0; v < verticales.size(); v=v+1)
+	  {
+					int y=verticales[v];			
+					if(v+1 < verticales.size())
+						if((verticales[v+1]-y) <15) v++;
 
+					for( int i = 0; i < 25; i++){
+						for( int j = 0; j < 16; j++){
+
+							if(x+i-3 < drawing.rows-1   && x+i-3 >= 0 && y+i-3 < drawing.cols-1  && y+i-3>=0){
+								ventana.at<uchar>(i,j) = drawing.at<uchar>(x+i-3,y+j-3);
+								drawing.at<uchar>(x+i-3,y+j-3)=255;
+							}
 						}
 					}
-				}
-				waitKey(0);
+					double min, max;
+					int maxfinal = 0;
+					int word = 0;
+
+					for(int t1=0 ;t1 < abc.size(); t1++){
+						matchTemplate( ventana, abc[t1], result, 4 );
+						minMaxLoc(result, &min, &max);
+						if(maxfinal <= max) {
+							maxfinal= max;
+							word=t1;
+						}
+					}
+
+					if( maxfinal >= 0.8 ) 
+					{
+						string text = probando.substr(word,1);
+						int fontFace = FONT_HERSHEY_SIMPLEX;
+						double fontScale = 0.7;
+						int thickness = 2;  
+						cv::Point textOrg(y,x+10);
+						cv::putText(src, text, textOrg, fontFace, fontScale, Scalar(255,54,66), thickness,8);
+						rectangle( src, Point(y-5,x-10), Point(y+15,x+30), color, 2, 8, 0 );
+					}
+		
+	  }
+	  /// Show in a window
+	  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+	  imshow( "Contours", src );
+	  imshow( "lineas", lineas );
+	}
+}
+
+
+vector<int> buscarhorizontales(vector<Point2f> puntos){
+
+	vector<vector<Point2f>> ghor;
+	vector<int> poshorizontales;
+
+	while (puntos.size()>0){
+		
+		vector<Point2f> horizontal;
+		int promedio=puntos[0].y;
+
+		poshorizontales.push_back(promedio);
+		horizontal.push_back(puntos[0]);
+		line( lineas, Point( 0, promedio ), Point( src.cols, promedio ), Scalar( 125 ),  1, 8 );
+		puntos.erase(puntos.begin() + 0);
+		
+
+		for(int i =0 ;i<puntos.size();i++){
+			
+			if( puntos[i].y < promedio+3 && puntos[i].y > promedio-3 ){
+					horizontal.push_back(puntos[i]);
+					puntos.erase(puntos.begin() + i);
+					i--;
 			}
 		}
-  }
+		ghor.push_back(horizontal);
+	}
+	sort(poshorizontales.begin(),poshorizontales.end());
+	//cout <<"Horizontales:"<< ghor.size()<<endl;
+	//return ghor;
+	return poshorizontales;
+}
+
+vector<int> buscarverticales(vector<Point2f> puntos){
+
+	vector<vector<Point2f>> gver;
+	vector<int> posverticales;
+
+	while (puntos.size()>0){
+		
+		vector<Point2f> vertical;
+		int promedio=puntos[0].x;
+
+		posverticales.push_back(promedio);
+		vertical.push_back(puntos[0]);
+		line( lineas, Point( promedio, 0 ), Point( promedio, src.cols), Scalar( 125 ),  1, 8 );
+		puntos.erase(puntos.begin() + 0);
+		
+		for(int i =0 ;i<puntos.size();i++){
+			
+			if( puntos[i].x < promedio+3 && puntos[i].x > promedio-3 ){
+					vertical.push_back(puntos[i]);
+					puntos.erase(puntos.begin() + i);
+					i--;
+			}
+		}
+		gver.push_back(vertical);
+	}
+	sort(posverticales.begin(),posverticales.end());
+	/*for(int i =0 ;i<posverticales.size();i++){
+		cout<<posverticales[i]<<endl;
+	}
+	*/
+	//cout <<"verticales:"<< gver.size()<<endl;
+	//return gver;
+	return posverticales;
+}
+
+
+void getdb(vector<Mat> &abc)
+{
+  Mat temp,temp_g;
   
-
- // matchTemplate( dst, template1, result, 5 );
- //imshow("Imagen5", result );
-
-  imshow("Imagen77", img_g );
-
-
-  waitKey(0);
-  return 0;
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//a.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//b.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//c.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//d.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//e.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//f.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//g.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//h.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//i.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//j.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//k.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//l.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//m.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//n.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//o.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//p.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//q.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//r.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//s.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//t.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//u.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//v.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//w.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//x.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//y.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  temp= imread( "C://Users//Manuela//Desktop//inf//vision por comptuador//proyectofinal//z.jpg" );
+  cvtColor( temp, temp, COLOR_BGR2GRAY );
+  abc.push_back(temp);
+  return;
 }
